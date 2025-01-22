5717ccbbc50e8a1aab5b1130c02f9869142035a4
javidwaldron <javidwaldron@gmail.com>
2025-01-22 12:09:52 -0500

    Javid early additions
    Changed scene order to main menu, added  start game, key remapping, and  exit  app

---
+++ 2dPlatformeer/main_menu.gd
@@ -0,0 +1,76 @@
+extends Control
+
+@onready var cursor = $CursorLayer/Cursor
+@onready var pause_menu = $GUI/InputSettings
+
+
+func _on_start_game_pressed():
+	get_tree().change_scene_to_file("res://game_singleplayer.tscn")
+
+
+
+
+
+
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
+
+
+func _on_controls_pressed():
+	$CanvasLayer/InputSettings.visible = true
+	$MarginContainer.visible = false
+
+
+
+
+
+
+
+
+func _on_coins_available_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
+
+
+
+
+func _on_settings_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
+
+
+
+
+
+func _on_delete_save_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+
+
+
+
+
+
+
+func _on_exit_game_pressed():
+	get_tree().quit()
